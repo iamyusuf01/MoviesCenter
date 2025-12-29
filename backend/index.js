2cea107c03029f53c84562a5946013b1ffc39cf8
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2025-12-29 23:56:08 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -3,36 +3,24 @@ import "dotenv/config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/database.js";
-import userRouter from "./routes/userRouter.js";
-import movieRouter from "./routes/movieRouter.js";
-import adminRouter from "./routes/adminRouter.js";
+import userRouter from './routes/userRouter.js'
+import movieRouter from './routes/movieRouter.js'
+import adminRouter from './routes/adminRouter.js'
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 //MongoDB Connect
 connectDB();
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-
-const allowedOrigins = process.env.CORS_ORIGIN
-  ? process.env.CORS_ORIGIN.split(",")
-  : [];
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // allow requests with no origin (Postman, server-to-server)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      console.error("CORS blocked:", origin);
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
