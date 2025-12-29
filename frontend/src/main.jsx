6cf6a84dc6eff16ce16a1b0c8748e780c10c1dd6
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2025-12-29 23:20:29 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,11 @@ import { BrowserRouter } from "react-router";
 import { AppContextProvider } from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+
     <BrowserRouter>
       <AppContextProvider>
         <App />
       </AppContextProvider>
     </BrowserRouter>
-  </StrictMode>
+
 );
