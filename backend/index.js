d752f0dfe88d0657032477501b06c3dc457bc6e9
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2025-12-30 00:06:59 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -3,27 +3,20 @@ import "dotenv/config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/database.js";
-import userRouter from './routes/userRouter.js'
-import movieRouter from './routes/movieRouter.js'
-import adminRouter from './routes/adminRouter.js'
-
+import userRouter from "./routes/userRouter.js";
+import movieRouter from "./routes/movieRouter.js";
+import adminRouter from "./routes/adminRouter.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 //MongoDB Connect
 connectDB();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: 'https://movies-center-frontend.vercel.app',
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
